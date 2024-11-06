107c4150773ebf7bdeb4d9471bfd05e80c4edea1
Zhaoyu Jin <jin364@purdue.edu>
2024-11-06 16:38:05 -0500

    Added README; Adjusted some configs/stats

--- run_mcpat.sh
+++ run_mcpat.sh
@@ -1,6 +1,9 @@
 gem5_out_dir='../gem5_with_mcpat/runahead_gem5/m5out'
 parser_dir='./gem5_mcpat_parser'
 mcpat_input_path='./McPAT_input/mcpat_in.xml'
+output_dir='./results/'${3-'McPAT_out'}
 python3 ${parser_dir}/Gem5McPATParser_custom.py -c ${gem5_out_dir}/config.json -s ${gem5_out_dir}/stats.txt -t ${parser_dir}/template_x86.xml \
         -o ${mcpat_input_path} \
-&& ./mcpat -infile ${mcpat_input_path} -print_level 1 -opt_for_clk 0
+&& ./mcpat -infile ${mcpat_input_path} -print_level $1 -opt_for_clk $2 | tee ${output_dir}
+
+echo "Results in $output_dir"
